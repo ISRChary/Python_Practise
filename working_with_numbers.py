90b808b8de3a9e4004e3dc8f767caaf5d3af5193
ISRChary <isrchary@gmail.com>
2024-04-11 17:12:25 +0530

    working with numbers added

---
+++ working_with_numbers.py
@@ -0,0 +1,37 @@
+# methods of numbers
+
+# importing math module 
+
+from math import *
+
+
+
+# abs (absolute value oa a number)
+absolute = -123  
+print("abs")
+print(abs(absolute))
+
+# pow (power method)
+# two parameters number and the power4
+print("power")
+print(pow(2,3))
+
+# max (maximum value in the given numbers)
+print("maximum")
+print(max(2,3,5,3,34,5,35,2345))
+
+# round (round to the nearest number)
+print("round")
+print(round(2.6))
+print(round(2.2))
+print(round(2.5))
+
+
+# floor from math
+print("floor")
+print(floor(3.5))
+
+
+# sqrt from math
+print("square root")
+print(sqrt(225))
