0e5aa94ebf339584db707fe0d11844f2afda67c6
ISRChary <isrchary@gmail.com>
2024-04-11 17:43:44 +0530

    list methods program

--- lists.py
+++ lists.py
@@ -7,13 +7,26 @@ for i in fruits:
 
 # list with integer values
 
-list = ["54","34","86","12","56","87","45","34"]
+list = [54,45,34,3,56,76,86,865,34,6557,973]
+# we can create a number list without the double inverted commas also like  list[2,4,5,6,7,6]
 print(list[3])
 print(list[-4])
 print(list[3:7])
-print(list.append(999))
+list.append(999)
+print(list.count("34"))
 print(list)
-print(list.reverse)
-print(list.insert(3,786))
-print(list.sort)
+list.reverse()
+print(list)
+list.insert(3,786)
+list.sort()
+print(list)
+print(list.index(34))
+list3 = list.copy()
+print(list3)
+list.remove(54)
+list.pop()  #removes the last element in the list
+# list.clear()
 
+list2 =[1,3,4,5,6]
+list.extend (list2)
+print(list2)
